9e90abef4d30832d09bdd4e1b34bfb7042ed17d4
HuyDev <haquanghuydz1996@gmail.com>
2025-04-09 22:04:13 +0700

    add_fileweb_ngiet

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = 'Nguyệt Đụt';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Birthday';
+const titleCard = 'Tặng Nguyệt Đụt';
+const contentCard = 'Gửi Nguyệt, chúc bạn sinh nhật vui vẻ, xinh gái, làm ăn phát đạt, luôn mạnh khỏe, bình an và gặp nhiều may mắn trong cuộc sống. Mong ngiệt đụt kiếm được người mình thương! 🎂🎈'
+;
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'ngiet.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
